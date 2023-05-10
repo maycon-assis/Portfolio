2afe35e1a47ff0fd40e5370dcad7ca0733520be7
Maycon Assis <may.con.fa@hotmail.com>
2023-05-10 22:49:23 +1200

    done nav, main and tools.

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,7 @@
-import { AiFillLinkedin, AiFillGithub } from 'react-icons/ai'
+import { AiFillLinkedin, AiFillGithub, AiOutlineHtml5 } from 'react-icons/ai'
+import { SiTailwindcss, SiRedux, SiExpress, SiTypescript } from 'react-icons/si'
+import { FaReact, FaNodeJs } from 'react-icons/fa'
+import { TbSql } from 'react-icons/tb'
 
 const stackIcons = [
   'html-5.png',
@@ -15,7 +18,7 @@ const stackIcons = [
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center py-10">
+      <div className="flex flex-col items-center justify-center py-5">
         <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-cyan-500 font-lora ">Maycon Assis</h1>
         <h2 className="font-lora py-3">SOFTWARE DEVELOPER</h2>
         <img className="my-3 border-8 rounded-full h-32 md:h-48 lg:h-56 w-32 md:w-48 lg:w-56 " src="Maycon (3).jpg" alt="" />
@@ -23,13 +26,41 @@ const Home = () => {
           Heya/Ola 👋 An easygoing person who loves living life and seeing every experience as an opportunity to grow! Enough chitchat, lets get to it!
         </p>
       </div>
-      <div className="text-5xl flex justify-center gap-14">
+      <div className="text-5xl flex justify-center gap-6  text-gray-500 pb-12">
         <a href="https://www.linkedin.com/in/maycon-assis-nz/" target="_blank" rel="noopener noreferrer">
           <AiFillLinkedin />
         </a>
         <a href="https://github.com/maycon-assis" target="_blank" rel="noopener noreferrer">
           <AiFillGithub />
         </a>
+      </div>
+      <hr />
+      <div className="py-12">
+        <div className=" flex flex-col justify-center text-center gap">
+          <h3 className="pb-3 font-lora text-lg text-blue-900">FRONT-END TOOLS</h3>
+          <p className="py-3  leading-7 text-gray-700">Used to create web applications that are interactive, accessible, and engaging. These technologies have worked for me like a dream.</p>
+        </div>
+        <div className="py-3 text-5xl flex justify-center gap-6 text-blue-900 ">
+          <AiOutlineHtml5 />
+          <SiTailwindcss />
+          <FaReact />
+          <SiRedux />
+        </div>
+      </div>
+      <div className="py-12">
+        <div className=" flex flex-col justify-center text-center gap">
+          <h3 className="pb-3 font-lora text-lg text-orange-300">BACK-END TOOLS</h3>
+          <p className="py-3 leading-7 text-gray-700">
+            These tool combined, to me have been like a well-oiled machine, allowing for functionality as well as building REST APIs that enable efficient communication between the front end and
+            server-side components
+          </p>
+        </div>
+        <div className="py-3 text-5xl flex justify-center gap-6 text-orange-300 ">
+          <FaNodeJs />
+          <SiExpress />
+          <TbSql />
+          <SiTypescript />
+        </div>
       </div>
     </>
 
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
       <ul className="flex gap-12 sm:gap-28 md:gap-44 lg:gap-96 text-2xl sm:text-3xl md:text-4xl lg:text-5xl ">
         <li>
           <Link
-            className="text-gray-600 px-4 py-2 border-8 rounded-md  hover:text-orange-500 font-bold duration-300 font-lora ease-in-out cursor-pointer"
+            className="text-gray-600 px-4 py-2 border-8 rounded-md  hover:text-orange-300 font-bold duration-300 font-lora ease-in-out cursor-pointer"
             activeClass="active"
             to="work"
             spy={true}
@@ -18,7 +18,7 @@ function Navbar() {
         </li>
         <li>
           <Link
-            className="text-gray-600 px-4 py-2 border-8 rounded-md  hover:text-cyan-500 font-bold duration-300 font-lora ease-in-out cursor-pointer"
+            className="text-gray-600 px-4 py-2 border-8 rounded-md  hover:text-blue-900 font-bold duration-300 font-lora ease-in-out cursor-pointer"
             activeClass="active"
             to="Maycon-Assis-2023-Resume.pdf "
             spy={true}
