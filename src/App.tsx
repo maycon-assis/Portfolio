f7c8452b4410b4b76113c190ae1b31d82acd620c
Maycon Assis <may.con.fa@hotmail.com>
2023-05-10 18:15:14 +1200

    current state

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import Work from './Work'
 
 function App() {
   return (
-    <div>
-      <div className=" bg-gradient-to-l from-gray-200 to-gray-300">
+    <div className="flex flex-col">
+      <div className=" bg-gradient-to-l from-gray-100 to-gray-300">
         <Navbar />
         <Home />
       </div>
-      <div className=" bg-gradient-to-l from-gray-200 to-gray-300">
+      <div className=" bg-gradient-to-l from-gray-100 to-gray-300">
         <Work />
       </div>
     </div>
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,15 +2,15 @@ import { Link } from 'react-scroll'
 
 function Navbar() {
   return (
-    <nav className="flex justify-center items-center w-8/12 mx-auto h-56 ">
-      <ul className="flex gap-96 text-3xl">
-        <li className="text-gray-600 text-5xl hover:text-orange-500 font-bold duration-300 font-lora ease-in-out cursor-pointer">
+    <nav className="container mx-auto px-4 h-56 flex items-center justify-center">
+      <ul className="flex gap-96 lg:text-5xl ">
+        <li className="text-gray-600 hover:text-orange-500 font-bold duration-300 font-lora ease-in-out cursor-pointer">
           <Link activeClass="active" to="work" spy={true} smooth={true} duration={700}>
             WORK
           </Link>
         </li>
 
-        <li className=" text-5xl text-gray-600 hover:text-orange-500 font-bold duration-300 font-lora ease-in-out cursor-pointer">
+        <li className="l text-gray-600 hover:text-orange-500 font-bold duration-300 font-lora ease-in-out cursor-pointer">
           <a href="Maycon-Assis-2023-Resume.pdf " target="_blank" rel="noopener noreferrer">
             RESUME
           </a>
