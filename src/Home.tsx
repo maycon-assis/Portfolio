c5560231e058c741f2f6c1c470223a46f2a3cfb5
Maycon Assis <may.con.fa@hotmail.com>
2023-05-10 16:55:46 +1200

    added selfie pic

--- src/Home.tsx
+++ src/Home.tsx
@@ -14,9 +14,9 @@ const Home = () => {
   return (
     <div className="flex flex-col gap-48 justify-start items-center h-screen">
       <div className="flex flex-wrap border-4 justify-around flex-shrink-0 items-center border-gray-600 w-8/12 h-3/6 shadow-lg ">
-        <h1 className=" text-8xl text-gray-600 w-1/2 font-lora">Maycon Assis</h1>
+        <h1 className=" text-8xl text-gray-600 w-1/2 font-lora ">Maycon Assis</h1>
         <a href="https://github.com/maycon-assis" target="_blank">
-          <img className=" h-24" src="github.png" alt="" />
+          <img className=" border-4 border-gray-600 rounded-full h-56" src="Maycon (3).jpg" alt="" />
         </a>
         <p className="  text-gray-600 text-2xl w-1/2 font-robo">
           After stumbling upon a programming paper during my engineering degree, I was immediately drawn to the world of software engineering. Fully immersing myself in the art of building software, I
@@ -24,9 +24,14 @@ const Home = () => {
           it fuels my curiosity and expands my knowledge. When I'm not at my keyboard, you can find me jamming away on my guitar, as music is an undying passion that is an inseparable part of who I
           am. 🎵
         </p>
-        <a href="https://www.linkedin.com/in/maycon-assis-nz/" target="_blank">
-          <img className="h-24" src="linkedin.png"></img>
-        </a>
+        <div className="flex gap-12">
+          <a href="https://www.linkedin.com/in/maycon-assis-nz/" target="_blank">
+            <img className="h-12" src="linkedin.png"></img>
+          </a>
+          <a href="https://github.com/maycon-assis" target="_blank">
+            <img className=" h-12" src="github.png" alt="" />
+          </a>
+        </div>
       </div>
       <div className="flex justify-around items-center shadow-lg flex-shrink-0 bg-gray-300 l h-48 w-8/12 border-4 border-gray-600 ">
         {stackIcons.map((icon, i) => (
--- src/Work.tsx
+++ src/Work.tsx
@@ -7,7 +7,7 @@ function Work() {
         <img className="border-4 border-gray-500 shadow-2xl" src="Todo.png" alt="Todo-image" />
       </div>
       <div className=" flex flex-col justify-around flex-wrap items-center w-1/2">
-        <h2 className=" text-6xl text-gray-500 font-lora">TODO</h2>
+        <h2 className=" text-6xl text-gray-600 font-lora">TODO</h2>
         <p className="text-3xl text-gray-600 w-3/4 font-robo">
           This is a brief description of a minimalist to-do list app that incorporates full-stack development and features like Redux and AsyncThunk. The app was built using React for the front-end,
           Express for the back-end, and Sqlite for the database. The project provided an opportunity to learn and work with new tools and frameworks while building a functional and practical
@@ -26,7 +26,7 @@ function Work() {
         <img className="border-4 border-gray-500 h-4/5 shadow-2xl" src="sipMain.png" alt="Todo-image" />
       </div>
       <div className=" flex flex-col justify-evenly flex-wrap items-center w-1/2">
-        <h2 className=" text-6xl text-gray-500 font-lora">SIP(SELF-IMPROVEMENT)</h2>
+        <h2 className=" text-6xl text-gray-600 font-lora">SIP(SELF-IMPROVEMENT)</h2>
         <p className="text-3xl text-gray-600 w-3/4 font-robo">
           In collaboration with a team of five, I developed a task manager app called SIP. It gamified self-improvement by allowing users to track progress and visualize goals with accurate time
           representations and intuitive charts. As the Product Owner, I proposed the concept and focused on backend development, setting up and maintaining the database, API, and server-side routes. I
