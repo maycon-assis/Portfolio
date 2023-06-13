f831470aaec947551da6711dd0157f37dabe1dff
Maycon Assis <may.con.fa@hotmail.com>
2023-06-13 22:27:26 +1200

    update cv

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -13,7 +13,7 @@ function Navbar() {
           </Link>
         </li>
         <li>
-          <a href="Maycon-Assis-Resume.pdf " target="_blank">
+          <a href="Maycon-Assis-2023-Resume.pdf " target="_blank">
             <motion.button className="text-gray-600 px-4 py-2 border-8 rounded-md  hover:text-blue-900 font-bold duration-300 font-lora ease-in-out cursor-pointer" whileHover={{ scale: 1.1 }}>
               RESUME
             </motion.button>
